a6c690512863fe6181edf35f12199a46895a0cb8
HibatAllah5 <hibakhaldoune5@gmail.com>
2024-01-10 22:46:06 +0100

    resd user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,12 @@
 -- Creates the database hbtn_0d_2 and the user user_0d_2
--- Creates a database
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
---Creates a user
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
---Grant SELECT privileges to a user
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost; 
+-- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+   ON `hbtn_0d_2`.*
+   TO 'user_0d_2'@'localhost'
+   IDENTIFIED BY 'user_0d_2_pwd';
+FLUSH PRIVILEGES;
